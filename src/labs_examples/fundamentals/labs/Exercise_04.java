d17d3b22b0437c793bd9f0d6460c2ec2d006be1d
nicksinopoli <adsinopo@ncsu.edu>
2022-02-08 13:08:19 -0500

    Completed

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -17,6 +17,17 @@ public class Exercise_04 {
 
         // write your code below
         char charval = 'a';
+        System.out.println("Charval's value is " + charval);
+        double trouble = 12.97843;
+        System.out.println("A double can hold larger values than a float, like " + trouble);
+        float bee = (float) 1.7;
+        System.out.println("Float is still useful. It holds smaller values such as " + bee);
+        long longVal = 122398;
+        System.out.println("Long holds the largest integer value, including as big as " + longVal);
+        byte small = 127;
+        System.out.println("The byte data type is small, holding numbers up to " +127);
+        short stack = 16;
+        System.out.println("Short is a little larger, having a size up to " + stack + " bits.");
 
     }
 
