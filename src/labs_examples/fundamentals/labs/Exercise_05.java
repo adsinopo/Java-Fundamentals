6516f1512d6b7279083f7e5f637d1afa13ab9a00
nicksinopoli <adsinopo@ncsu.edu>
2022-02-08 15:39:53 -0500

    Finished Exercise 5

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,15 +14,18 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
-
-        String str2 = "hello";
+        int x = str.length();
+        String str2 = "Will the real slim shady please stand up?";
         // please initialize a boolean variable and test whether str is equal to str2
-
+        boolean equals = "hello".equals(str2);
+        System.out.println("Do str1 and str2 match? " + equals);
         // please concatenate str & str2 and set the result to a new String variable below
-
+        String str3 = "Hello, " + "the value of x is: " + x + "!";
+        System.out.println(str3);
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
-
+        int search = "hello!".indexOf("e");
+        System.out.println(search);
     }
 
 
