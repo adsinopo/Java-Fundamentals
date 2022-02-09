8777fe673e85e52961f8fc50d2d32711b5a40eca
nicksinopoli <adsinopo@ncsu.edu>
2022-02-09 10:54:20 -0500

    added color

--- src/labs_examples/fundamentals/examples/VariableScope.java
+++ src/labs_examples/fundamentals/examples/VariableScope.java
@@ -12,10 +12,23 @@ public class VariableScope {
         myCar.make = "Mazda";
         myCar.model = "Mazda 6 Touring";
         myCar.year = 2014;
+        myCar.color = "Blue";
 
         if (myCar.make.equalsIgnoreCase("Mazda")){
             String message = "Zoom, zoom!";
+            System.out.println("Their slogan is " + message);
+
+        }
+        if (myCar.color.equalsIgnoreCase("Blue")){
+            String message = "The color resembles the ocean.";
             System.out.println(message);
+
+        }
+        if (myCar.model.equalsIgnoreCase("Mazda 6 Touring")){
+            String message = "My car is a Mazda.";
+            System.out.println(message);
+
+
         }
     }
 
