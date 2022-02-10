6ddd160be90185db383a46b3442f750d1ceffbea
nicksinopoli <adsinopo@ncsu.edu>
2022-02-09 23:11:01 -0500

    finished exercise 6

--- src/labs_examples/datatypes_operators/labs/Exercise_06.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_06.java
@@ -13,6 +13,16 @@ public class Exercise_06 {
     public static void main(String[] args) {
 
         // write code here
+        double radius = 3.14;
+        int height = 5;
+        double radiusSquared = radius * radius;
+        double valueOfpi = 3.1415926;
+        double volume = valueOfpi * radiusSquared * height;
+        double surfaceArea = ((2 * valueOfpi * radius * height) + (2 * valueOfpi * radiusSquared));
+        //volume result
+        System.out.println(volume);
+        //surface area result
+        System.out.println(surfaceArea);
 
     }
 }
