1a800432eaba678096f5aa15415d1f3523cd4eae
nicksinopoli <adsinopo@ncsu.edu>
2022-02-09 22:53:20 -0500

    finished exercise 3

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -11,6 +11,16 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        int addition = 2 + 10;
+        System.out.println(addition); //prints 12
+        int subtract = 14 - 3;
+        System.out.println(subtract); //prints 11
+        int multiply = 6 * 7;
+        System.out.println(multiply); //prints 42
+        int divide = 35 / 5;
+        System.out.println(divide); //prints 7
+        int mod = 14 % 4;
+        System.out.println(mod); //prints 2
 
     }
 
