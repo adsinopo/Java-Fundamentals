46df77a9d2327dff30c0891ea46ed6c2fed00376
nicksinopoli <adsinopo@ncsu.edu>
2022-02-11 11:38:52 -0500

    While and Do While Challenges

---
+++ src/labs_examples/conditionalpractice/DeleteMe.java
@@ -0,0 +1,18 @@
+package labs_examples.conditionalpractice;
+
+public class DeleteMe {
+
+    public static void main(String[] args) {
+
+
+        int count = 0;
+        int val = 0;
+
+        while (count < 10) {
+            val += count;
+            count++;
+        }
+        System.out.println("val = " + val);
+
+        }
+    }
