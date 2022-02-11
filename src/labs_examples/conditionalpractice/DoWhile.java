f8c6b31dd28aa7949aa89c1b59604393e4df017a
nicksinopoli <adsinopo@ncsu.edu>
2022-02-11 09:37:43 -0500

    more loops practice

--- src/labs_examples/conditionalpractice/DoWhile.java
+++ src/labs_examples/conditionalpractice/DoWhile.java
@@ -1,2 +1,17 @@
-package labs_examples.conditionalpractice;public class DoWhile {
+package labs_examples.conditionalpractice;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        do {
+            System.out.println("count is " + count);
+            count++;
+        } while (count < 10);
+
+        System.out.println("all done with do-while loop");
+
+    }
 }
--- src/labs_examples/conditionalpractice/LoopExample.java
+++ src/labs_examples/conditionalpractice/LoopExample.java
@@ -1,2 +1,16 @@
-package labs_examples.conditionalpractice;public class LoopExample {
+package labs_examples.conditionalpractice;
+
+public class LoopExample {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        while(count < 10){
+            System.out.println("count is " + count);
+            count++;
+        }
+        System.out.println("all done with while loop!");
+
+    }
 }
