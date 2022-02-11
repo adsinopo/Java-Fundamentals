6173ae32444009520fadfd75d6bd06a962b60824
nicksinopoli <adsinopo@ncsu.edu>
2022-02-11 09:44:38 -0500

    loop prac

--- src/labs_examples/conditionalpractice/LoopExample.java
+++ src/labs_examples/conditionalpractice/LoopExample.java
@@ -4,11 +4,17 @@ public class LoopExample {
 
     public static void main(String[] args) {
 
-        int count = 0;
+        int outterCount = 0;
+        int innerCount = 0;
 
-        while(count < 10){
-            System.out.println("count is " + count);
-            count++;
+        while(outterCount < 10) {
+
+            while (innerCount < 10) {
+                System.out.println("outer count = " + outterCount + " and inner count = " + innerCount);
+                innerCount++;
+            }
+            innerCount = 0;
+            outterCount++;
         }
         System.out.println("all done with while loop!");
 
