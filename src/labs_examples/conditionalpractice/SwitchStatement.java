63d8e06280295ba05c86b736f6adc612de86651f
nicksinopoli <adsinopo@ncsu.edu>
2022-02-12 22:21:55 -0500

    switch statement practice

---
+++ src/labs_examples/conditionalpractice/SwitchStatement.java
@@ -0,0 +1,53 @@
+package labs_examples.conditionalpractice;
+
+public class SwitchStatement {
+
+    public static void main(String[] args) {
+
+        // please demonstrate the use of a switch statement below - have a "System.out.println()"
+        // statement print for each "case" - be sure to include "break" statements in each case
+        int month = 6;
+
+        switch(month) {
+            case 1 :
+                System.out.println("It's January");
+                break;
+            case 2 :
+                System.out.println("It's February");
+                break;
+            case 3 :
+                System.out.println("It's March");
+                break;
+            case 4 :
+                System.out.println("It's April");
+                break;
+            case 5 :
+                System.out.println("It's May");
+                break;
+            case 6 :
+                System.out.println("It's June");
+                break;
+            case 7 :
+                System.out.println("It's July");
+                break;
+            case 8 :
+                System.out.println("It's August");
+                break;
+            case 9 :
+                System.out.println("It's September");
+                break;
+            case 10 :
+                System.out.println("It's October");
+                break;
+            case 11 :
+                System.out.println("It's November");
+                break;
+            case 12 :
+                System.out.println("It's December");
+                break;
+        }
+        // once you've got that done - run the switch statement with and
+        // without the break statements for each case - what happens when we don't have break statements?
+
+    }
+}
