dad57eb2d011fd239e7cee41c660dc8e521fd06a
nicksinopoli <adsinopo@ncsu.edu>
2022-02-12 21:49:07 -0500

    use for each loop practice for later

---
+++ src/labs_examples/conditionalpractice/ForEachLoop.java
@@ -0,0 +1,14 @@
+package labs_examples.conditionalpractice;
+
+public class ForEachLoop {
+
+    public static void main(String[] args) {
+
+        for(String val : args){
+            System.out.println(val);
+        }
+
+    }
+
+
+}
