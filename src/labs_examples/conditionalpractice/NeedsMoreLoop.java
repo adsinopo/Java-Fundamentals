5986bb7000c7a1d8d71a50d2384dc5bfd213d12d
nicksinopoli <adsinopo@ncsu.edu>
2022-02-12 21:46:10 -0500

    loop practice

--- src/labs_examples/conditionalpractice/NeedsMoreLoop.java
+++ src/labs_examples/conditionalpractice/NeedsMoreLoop.java
@@ -1,2 +1,16 @@
-package labs_examples.conditionalpractice;public class NeedsMoreLoop {
+package labs_examples.conditionalpractice;
+
+public class NeedsMoreLoop {
+
+    public static void main(String[] args) {
+        int val = 0;
+
+        for(int i = 0; i <= 100; i++){
+
+            val += i;
+        }
+        System.out.println(val);
+    }
+
+
 }
