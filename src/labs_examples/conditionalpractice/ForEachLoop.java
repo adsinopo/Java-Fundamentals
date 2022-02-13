c8d5d8fdd00379bae6141a7a5ec9eeaaa81f99f9
nicksinopoli <adsinopo@ncsu.edu>
2022-02-12 21:55:27 -0500

    nested loops

--- src/labs_examples/conditionalpractice/ForEachLoop.java
+++ src/labs_examples/conditionalpractice/ForEachLoop.java
@@ -4,9 +4,19 @@ public class ForEachLoop {
 
     public static void main(String[] args) {
 
-        for(String val : args){
-            System.out.println(val);
+//        for(String val : args){
+//            System.out.println(val);
+//        }
+        System.out.println("Starting...");
+        for (int i = 0; i < 10; i++){
+            System.out.println("** Outer Loop - i = " + i + " **");
+            for (int j = 0; j < 7; j++){
+                System.out.print("Inner Loop: j = " + j + " | ");
+            }
+            // this is just two line breaks
+            System.out.println("\n\n");
         }
+        System.out.println("Done!");
 
     }
 
