6379667261974bf3816c0e586ae85b20a8adb3b6
nicksinopoli <adsinopo@ncsu.edu>
2022-02-12 21:45:40 -0500

    for loops

--- src/labs_examples/conditionalpractice/ForLoop.java
+++ src/labs_examples/conditionalpractice/ForLoop.java
@@ -8,14 +8,27 @@ public class ForLoop {
     public static void main(String[] args) {
 
         // for(initialization; (boolean); iterator)
-        for(int i = 0; i < 10; i += 2){
-            System.out.println("i = " + i);
-//            for(int x = 0; x < 10; x++){
-//                System.out.println("--x = " + x);
-            // this prints out x = 0.....for every time i prints
+        // for (int i = 0; i < 10; i += 2) {
+        //    System.out.println("i = " + i);
+
+        // please use a for loop below to print out every number between 0 & 100
+        for (int a = 0; a < 100; a++) {
+            System.out.println("a = " + a);
+        }
+//        // please use another for loop below to print out every odd number between 100 and 0 in descending order
+        for (int b = 100; b >= 0; b--) {
+            if (b % 2 != 0) {
+                System.out.println("b = " + b);
             }
+            // pleasure demonstrate "nesting" two for loops below
+
+        }
+        for (int c = 0; c < 5; c++) {
+            System.out.println("c = " + c);
+            for (int d = 0; d < 5; d++){
+                System.out.println("But now d = " + d);
+            }
+
         }
     }
-
-
-
+}
