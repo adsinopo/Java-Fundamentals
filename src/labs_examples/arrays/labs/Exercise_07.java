60edb3c818c05471a661f374edd8588023801d69
nicksinopoli <adsinopo@ncsu.edu>
2022-02-21 13:40:26 -0500

    finished exercise 7. 6 needs work

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,5 +1,8 @@
 package labs_examples.arrays.labs;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *  ArrayLists
  *
@@ -10,4 +13,38 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_07 {
+    public static void main(String[] args) {
+        ArrayList<String> roster = new ArrayList<>();
+
+        roster.add("Sam");
+        roster.add("Caleb");
+        roster.add("Nick");
+        roster.add("Tony");
+        roster.add("Kasey");
+        roster.add("Holly");
+        roster.add("Victoria");
+        roster.add("Oliver");
+        roster.add("Benjamin");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type in your name: ");
+
+        System.out.println(roster.indexOf("Oliver"));
+        System.out.println(roster.get(2));
+
+        // The name Victoria is removed from the list
+        // Oliver would now exist at index 6
+        roster.remove(6);
+        System.out.println(roster.indexOf("Oliver"));
+
+        String name = scanner.next();
+
+        boolean check = roster.contains(name);
+            if (check != true){
+                System.out.println("That name is not on the list.");
+            } else {
+                System.out.println("That name is on the list!");
+            }
+
+    }
 }
