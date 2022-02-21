14649f59ee70af238b375a427858a1e8969f11a2
nicksinopoli <adsinopo@ncsu.edu>
2022-02-21 10:51:07 -0500

    finished

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -10,4 +10,29 @@ package labs_examples.arrays.labs;
 
 public class Exercise_04 {
 
+    public static void main(String[] args) {
+
+        int[][] irregArray = {
+                {10, 9},
+                {78, 109, 123},
+                {156, 8, 402, 99},
+        };
+
+        // This is the method for a "nested for loop"
+//        for (int i = 0; i < irregArray.length; i++){
+//            for (int j = 0; j < irregArray[i].length; j++){
+//                System.out.print(irregArray[i][j] + " - ");
+//            }
+//            System.out.println();
+//        }
+
+        //This is how to do it with a "nested for-each loop"
+        for (int [] val : irregArray){
+            for (int j : val){
+                System.out.print(j + " - ");
+            }
+            System.out.println();
+        }
+
+    }
 }
