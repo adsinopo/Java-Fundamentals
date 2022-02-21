25da0c222b167c858a4ab2aec86695c2bde2ce55
nicksinopoli <adsinopo@ncsu.edu>
2022-02-20 21:28:44 -0500

    solid progress

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  * Arrays calculator
  *
@@ -12,4 +14,32 @@ package labs_examples.arrays.labs;
 
 public class Exercise_01 {
 
+    public static void main(String[] args) {
+
+        int myArray [] = new int[10];
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        myArray[0] = scanner.nextInt();
+        myArray[1] = scanner.nextInt();
+        myArray[2] = scanner.nextInt();
+        myArray[3] = scanner.nextInt();
+        myArray[4] = scanner.nextInt();
+        myArray[5] = scanner.nextInt();
+        myArray[6] = scanner.nextInt();
+        myArray[7] = scanner.nextInt();
+        myArray[8] = scanner.nextInt();
+        myArray[9] = scanner.nextInt();
+
+        int sum = 0;
+        double average = 000.00;
+        for (int i = 0; i < myArray.length; i++){
+            sum += myArray[i];
+        }
+        System.out.println("sum is " + sum);
+
+        average = (sum) / myArray.length;
+        System.out.println("The average is " + average);
+
+    }
 }
