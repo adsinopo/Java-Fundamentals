7376648392b6a76fc9f4e4513a99f4ce596527f0
nicksinopoli <adsinopo@ncsu.edu>
2022-02-23 14:45:41 -0500

    student code challenge

--- src/labs_examples/class_practice/Student.java
+++ src/labs_examples/class_practice/Student.java
@@ -1,2 +1,20 @@
-package labs_examples.class_practice;public class Student {
+package labs_examples.class_practice;
+
+public class Student {
+
+    String name;
+    int age;
+    String[] courses = new String[2];
+
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "Bob";
+        student.age = 34;
+        student.courses[0] = "Java";
+        student.courses[1] = "Python";
+        System.out.println("Hello, I'm " + student.name + ", I'm " + student.age + " and I'm learning " + student.courses[0] + " & " + student.courses[1]);
+
+    }
+
 }
