c95b164d55c533a2ae9efc1eb3a634ff3353dfb5
nicksinopoli <adsinopo@ncsu.edu>
2022-02-24 14:54:24 -0500

    calling a method of my own

--- src/labs_examples/class_practice/MethodPractice.java
+++ src/labs_examples/class_practice/MethodPractice.java
@@ -1,2 +1,18 @@
-package labs_examples.class_practice;public class MethodPractice {
+package labs_examples.class_practice;
+
+public class MethodPractice {
+
+    public static void main(String[] args) {
+        System.out.println("It starts here");
+        printMethod();
+        System.out.println("And now we are done");
+    }
+
+    public static String printMethod(){
+        System.out.println("Then enters here");
+        String method = "printMethod() called!";
+        System.out.println(method);
+        return method;
+    }
+
 }
