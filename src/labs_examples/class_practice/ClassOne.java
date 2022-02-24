bfb9d48963888147596b68661618b8b0f241d7ae
nicksinopoli <adsinopo@ncsu.edu>
2022-02-24 14:30:25 -0500

    class practice

--- src/labs_examples/class_practice/ClassOne.java
+++ src/labs_examples/class_practice/ClassOne.java
@@ -5,8 +5,30 @@ public class ClassOne {
     // ACCESS MODIFIERS - affect classes, instance variables and methods they play a large role in
     // Encapsulation - one of the primary tenants of Object-Oriented Programming
 
-    public int publicVar;
-    protected int protectedVar;
-    private int privateVar;
+//    public int publicVar;
+//    protected int protectedVar;
+//    private int privateVar;
 
-}
+        public static void main(String[] args) {
+
+            AccessMod obj = new AccessMod();
+
+            // try to set each instance var on the "obj" object
+            // hint - only two will work
+
+            obj.publicVar = String.valueOf(12);
+            obj.protectedVar = String.valueOf(4);
+//            private doesn't work
+//            obj.privateVar = String.valueOf(9);
+            
+        }
+    }
+
+    class AccessMod {
+
+        public String publicVar;
+        protected String protectedVar;
+        private String privateVar;
+
+    }
+
