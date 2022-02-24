70ab436a0513f0c4634b9326d5fcdcc68d0d7dc1
nicksinopoli <adsinopo@ncsu.edu>
2022-02-24 13:05:00 -0500

    class practice

--- src/labs_examples/class_practice/ClassOne.java
+++ src/labs_examples/class_practice/ClassOne.java
@@ -1,2 +1,12 @@
-package labs_examples.class_practice;public class ClassOne {
+package labs_examples.class_practice;
+
+public class ClassOne {
+
+    // ACCESS MODIFIERS - affect classes, instance variables and methods they play a large role in
+    // Encapsulation - one of the primary tenants of Object-Oriented Programming
+
+    public int publicVar;
+    protected int protectedVar;
+    private int privateVar;
+
 }
--- src/labs_examples/class_practice/ClassTwo.java
+++ src/labs_examples/class_practice/ClassTwo.java
@@ -1,2 +1,18 @@
-package labs_examples.class_practice;public class ClassTwo {
+package labs_examples.class_practice;
+
+public class ClassTwo {
+
+    public static void main(String[] args) {
+        ClassOne classOne = new ClassOne();
+
+        classOne.publicVar = 10;
+        classOne.protectedVar = 20;
+        classOne.privateVar = 30;
+
+        public void changePrivateVar(int val){
+            privateVar = val;
+        }
+        
+    }
+
 }
