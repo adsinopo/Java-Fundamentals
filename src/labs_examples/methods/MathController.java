761b218c239aedbeafb0b95171072f10af5e10c5
nicksinopoli <adsinopo@ncsu.edu>
2022-02-24 20:49:30 -0500

    math exercise changes

--- src/labs_examples/methods/MathController.java
+++ src/labs_examples/methods/MathController.java
@@ -1,7 +1,19 @@
 package labs_examples.methods;
 
+import videos_source_code.methods.CustomMath;
+
 public class MathController {
 
+    public static void main(String[] args) {
 
+        int a = 2;
+        int b = 3;
+
+        int x = CustomMath.multiply(a, b);
+        int z = CustomMath.divide(4, 2);
+
+        System.out.println(x);
+        System.out.println(z);
+    }
 
 }
