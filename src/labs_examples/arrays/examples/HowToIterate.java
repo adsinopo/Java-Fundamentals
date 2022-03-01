6530a6630717db05e04b9d691ea529d02bb1a44f
nicksinopoli <adsinopo@ncsu.edu>
2022-02-28 21:19:14 -0500

    this is how to iterate an array

--- src/labs_examples/arrays/examples/HowToIterate.java
+++ src/labs_examples/arrays/examples/HowToIterate.java
@@ -1,2 +1,16 @@
-package labs_examples.arrays.examples;public class HowToIterate {
+package labs_examples.arrays.examples;
+
+public class HowToIterate {
+
+    public static void main(String[] args) {
+
+        int [] myArray = new int[5];
+        int count = 0;
+
+        for (int i = 0; i < myArray.length; i++){
+            count++;
+            System.out.print(count + " - ");
+        }
+    }
+
 }
