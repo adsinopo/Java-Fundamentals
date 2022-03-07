390c046be5c370ac0d6589b7154f35e7a54cbb8b
nicksinopoli <adsinopo@ncsu.edu>
2022-03-07 15:29:46 -0500

    inheritance practice

--- src/labs_examples/objects_classes_methods/labs/oop/practice/Vehicle.java
+++ src/labs_examples/objects_classes_methods/labs/oop/practice/Vehicle.java
@@ -5,25 +5,32 @@ public class Vehicle {
     int mpg;
     int fuel_capacity;
     String type;
-    // ...
+    // custom constructor replaces default constructor - to create a Vehicle object you
+    // must use this custom constructor and pass the required args
+    public Vehicle(int mpg, int fuel_capacity, String type){
+        this.mpg = mpg;
+        this.fuel_capacity = fuel_capacity;
+        this.type = type;
+    }
 }
 
 // child class Motorcycle
 class MotorCycle extends Vehicle {
     int cubicLiters;
-    // ...
+    // MotorCycle Constructor
+    MotorCycle(int cubicLiters, int mpg, int fuel_capacity, String type){
+        // call parent constructor first - always first - and pass required parameters
+        super(mpg, fuel_capacity, type);
+        // setl local variables
+        this.cubicLiters = cubicLiters;
+    }
 }
 
 // child class (of MotorCycle) Ducati
-class Ducati extends MotorCycle {
-    boolean dryClutch = true;
-    // constructor for Ducati
-    public Ducati(){
-        // the following variables below to Vehicle
-        mpg = 30;
-        fuel_capacity = 10;
-        // cubicLiters belongs to MotorCycle
-        cubicLiters = 2000;
+class MotoExample {
+    public static void main(String[] args) {
+        // Here we must use the custom constructor provided in the Motorcycle class,
+        // which calls "super" on the Vehicle class
+        MotorCycle bike = new MotorCycle(2000, 25, 16, "Ducati Monster");
     }
-    // ...
 }
