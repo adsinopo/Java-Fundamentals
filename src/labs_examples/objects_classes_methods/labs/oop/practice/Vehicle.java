1e94c5d376521dee61bc2df45c2e69e334ffecf9
nicksinopoli <adsinopo@ncsu.edu>
2022-03-07 13:43:26 -0500

    practicing with OOPS

--- src/labs_examples/objects_classes_methods/labs/oop/practice/Vehicle.java
+++ src/labs_examples/objects_classes_methods/labs/oop/practice/Vehicle.java
@@ -1,2 +1,29 @@
-package labs_examples.objects_classes_methods.labs.oop.practice;public class Vehicle {
+package labs_examples.objects_classes_methods.labs.oop.practice;
+
+//parent class vehicle
+public class Vehicle {
+    int mpg;
+    int fuel_capacity;
+    String type;
+    // ...
+}
+
+// child class Motorcycle
+class MotorCycle extends Vehicle {
+    int cubicLiters;
+    // ...
+}
+
+// child class (of MotorCycle) Ducati
+class Ducati extends MotorCycle {
+    boolean dryClutch = true;
+    // constructor for Ducati
+    public Ducati(){
+        // the following variables below to Vehicle
+        mpg = 30;
+        fuel_capacity = 10;
+        // cubicLiters belongs to MotorCycle
+        cubicLiters = 2000;
+    }
+    // ...
 }
