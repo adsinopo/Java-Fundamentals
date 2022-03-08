b09df9a463760c2179231c11b8ea9b971ece2640
nicksinopoli <adsinopo@ncsu.edu>
2022-03-08 13:01:17 -0500

    understanding interfaces

--- src/labs_examples/objects_classes_methods/labs/oop/practice/Dependency.java
+++ src/labs_examples/objects_classes_methods/labs/oop/practice/Dependency.java
@@ -1,2 +1,21 @@
-package labs_examples.objects_classes_methods.labs.oop.practice;public class Dependency {
+package labs_examples.objects_classes_methods.labs.oop.practice;
+
+public class Dependency {
+
+    MotorizedTransport transport;
+
+    public Dependency(MotorizedTransport transport){
+        this.transport = transport;
+    }
+
+    public void testMethods(){
+        transport.start();
+        transport.accelerate(20);
+        transport.stop();
+        System.out.println("-----------------");
+    }
+
+    public void setTransport(MotorizedTransport transport) {
+        this.transport = transport;
+    }
 }
--- src/labs_examples/objects_classes_methods/labs/oop/practice/DependencyInjectionController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/practice/DependencyInjectionController.java
@@ -1,2 +1,22 @@
-package labs_examples.objects_classes_methods.labs.oop.practice;public class DependencyInjectionController {
+package labs_examples.objects_classes_methods.labs.oop.practice;
+
+public class DependencyInjectionController {
+
+    public static void main(String[] args) {
+
+        Truck truck = new Truck("Ford", "F150", 120000, 500);
+        Sedan sedan = new Sedan("Honda", "Civic", 60000, 80);
+
+        DirtBike dirtBike = new DirtBike("Ducati", "Monster", 500, 1000, false);
+
+        Dependency example = new Dependency(truck);
+        example.testMethods();
+
+        example.setTransport(sedan);
+        example.testMethods();
+
+        example.setTransport(dirtBike);
+        example.testMethods();
+
+    }
 }
