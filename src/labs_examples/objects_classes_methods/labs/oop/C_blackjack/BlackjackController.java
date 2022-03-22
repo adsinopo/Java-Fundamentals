afb8d0014cc65e6aae0bd73c55a2c4e18d3a27cd
nicksinopoli <adsinopo@ncsu.edu>
2022-03-21 20:33:41 -0400

    more updates on main method

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
@@ -1,11 +1,29 @@
 package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
 import java.util.Arrays;
+import java.util.Scanner;
+
 public class BlackjackController {
     public static void main(String[] args) {
         Deck deck = new Deck();
 
-        Player player1 = new Player("Nick", 100);
-        Player player2 = new Player();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+
+        Player player1 = new Player(scanner.next(), 100);
+        Player computer = new Player("Computer", 100);
+
+        deck.deal(player1);
+        deck.deal(computer);
+        deck.deal(player1);
+        deck.deal(computer);
+
+        System.out.println(player1.getHand());
+        Scanner scanner2 =new Scanner(System.in);
+        deck.deal(player1);
+
+        System.out.println(computer.getHand());
+        deck.deal(computer);
+
 //        public void playBlackJack(Player){
 //
 //        }
