1a472c3b7b547441a8a204da8f44f4f39b6e3579
nicksinopoli <adsinopo@ncsu.edu>
2022-03-21 20:16:55 -0400

    back on blackjack

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
@@ -3,16 +3,23 @@ import java.util.Arrays;
 public class BlackjackController {
     public static void main(String[] args) {
         Deck deck = new Deck();
-        for(Card card : deck.cards){
-            System.out.println(card);
 
-        }
-        Player player1;
-        player1 = new Player("Jon", 100);
-        deck.deal(player1);
-        System.out.println(player1.getHand());
-        deck.deal(player1);
-        System.out.println(player1.getHand());
+        Player player1 = new Player("Nick", 100);
+        Player player2 = new Player();
+//        public void playBlackJack(Player){
+//
+//        }
+//        for(Card card : deck.cards){
+//            System.out.println(card);
+//
+//        }
+//        Player player1;
+//        player1 = new Player("Jon", 100);
+//        deck.deal(player1);
+//        Player player2 = new Player();
+//        System.out.println(player1.getHand());
+//        deck.deal(player1);
+//        System.out.println(player1.getHand());
 
     }
 }
