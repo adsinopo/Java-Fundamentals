66cea1694b9b894ec06f54addcab291505501f78
nicksinopoli <adsinopo@ncsu.edu>
2022-03-24 15:11:08 -0400

    woohoo the blackjack game is finished

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
@@ -9,22 +9,22 @@ public class BlackjackController {
     }
 
     public static void playBlackJack() {
-//        Deck deck = new Deck();
+        Deck deck = new Deck();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
         Player player1 = new Player(scanner.next(), 100);
         Player computer = new Player("Computer", 100);
 //        System.out.print("Enter your bet: ");
 
-//        Where do i put this???
+
         do{
-            Deck deck = new Deck();
+//            Deck deck = new Deck();
             Scanner scanner1 = new Scanner(System.in);
             System.out.print("Enter your bet: ");
             int playerBet;
             double computerBet;
-            playerBet = scanner1.nextInt();
-            computerBet = computer.potValue * .15;
+            playerBet = scanner.nextInt();
+            computerBet = playerBet;
             System.out.println("The computer bets " + computerBet);
 
             deck.deal(player1);
@@ -60,6 +60,10 @@ public class BlackjackController {
                 System.out.println("Congratulations " + player1.getName() + "! You win!");
                 player1.potValue += computerBet;
                 computer.potValue -= computerBet;
+            } else if (player1.hand.busted() && computer.hand.busted()){
+                System.out.println("Sorry you both lose! The house wins!");
+                computer.potValue -= computerBet;
+                player1.potValue -= playerBet;
             } else if (player1.hand.handValue < computer.hand.handValue && (!player1.hand.busted() && !computer.hand.busted())) {
                 System.out.println("Sorry " + player1.getName() + "! You lose this time....");
                 computer.potValue += playerBet;
@@ -71,8 +75,6 @@ public class BlackjackController {
             } else if (player1.hand.handValue == computer.hand.handValue && (!player1.hand.busted() && !computer.hand.busted())) {
                 System.out.println("You tied! Split pot.");
                 // No one wins any money
-//                computer.potValue -= computerBet;
-//                player1.potValue -= playerBet;
             } else {
                 System.out.println("Sorry you both lose! The house wins!");
                 computer.potValue -= computerBet;
@@ -81,8 +83,15 @@ public class BlackjackController {
             System.out.println("The computer has " + computer.potValue + " chips.");
             System.out.println("You have " + player1.potValue + " chips.");
 
-//            player1.hand.playerHand.remove();
-//            computer.hand.playerHand.remove();
+            player1.hand.playerHand.clear();
+            computer.hand.playerHand.clear();
+            deck.usedCards.clear();
+
         } while (player1.potValue > 0 && computer.potValue > 0);
+        if (player1.potValue == 0) {
+            System.out.println("Sorry you lost all of your money :(. Time to go to the ATM.");
+        } else {
+            System.out.println("You are victorious! You have " + player1.potValue + " chips and the computer is flat broke.");
+        }
     }
 }
