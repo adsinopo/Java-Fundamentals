4b85954bacf9b2af7acbed94d6953b8385a67b94
nicksinopoli <adsinopo@ncsu.edu>
2022-03-30 09:44:20 -0400

    calling methods

--- src/labs_examples/static_nonstatic/labs/ClassOne.java
+++ src/labs_examples/static_nonstatic/labs/ClassOne.java
@@ -1,2 +1,48 @@
-package labs_examples.static_nonstatic.labs;public class ClassOne {
+package labs_examples.static_nonstatic.labs;
+
+public class ClassOne {
+    public static void main(String[] args) {
+        // A static method calling another static method in the same class (call the method)
+        Nick();
+        // A static method calling a non-static method in the same class
+        // (create an object, use the dot, call the method)
+        ClassOne sameClass = new ClassOne();
+        sameClass.Jon();
+        // A static method calling a static method in another class
+        // call the class, call the method
+        int a = 12;
+        int b = 5;
+        int c = ClassTwo.addition(a, b);
+        System.out.println(c);
+        // A static method calling a non-static method in another class
+        // (create object, call the class, call the method)
+        ClassTwo differentClass = new ClassTwo();
+        int d = differentClass.division(a,b);
+        System.out.println(d);
+    }
+    public void nonStaticMethod(){
+        // A non-static method calling a non-static method in the same class
+        // (call the method)
+        Ryan();
+        // A non-static method calling a non-static method in another class
+        // (create an object in the new class, call the object, use the dot operator, call the method)
+        ClassTwo obj = new ClassTwo();
+        obj.division(10, 2);
+        // A non-static method calling a static method in the same class
+        // (just call the method)
+        Nick();
+        // A non-static method calling a static method in another class
+        // (call the class, use the dot operator, call the method)
+        ClassTwo.addition(10, 15);
+    }
+
+    public static void Nick(){
+        System.out.println("The Nick method is static");
+    }
+    public void Jon(){
+        System.out.println("The Jon method is non-static");
+    }
+    public void Ryan(){
+        System.out.println("Ryan is non-static");
+    }
 }
--- src/labs_examples/static_nonstatic/labs/ClassTwo.java
+++ src/labs_examples/static_nonstatic/labs/ClassTwo.java
@@ -1,2 +1,15 @@
-package labs_examples.static_nonstatic.labs;public class ClassTwo {
+package labs_examples.static_nonstatic.labs;
+
+public class ClassTwo {
+    public static void main(String[] args) {
+
+    }
+    public static int addition(int a, int b){
+        System.out.println("Addition method is static");
+        return a + b;
+    }
+    public int division(int a, int b){
+        System.out.println("division method is non static");
+        return a / b;
+    }
 }
