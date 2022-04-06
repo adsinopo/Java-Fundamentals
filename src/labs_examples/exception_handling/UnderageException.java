6fc9af018a29502b663caf81166741680772abdb
nicksinopoli <adsinopo@ncsu.edu>
2022-04-05 22:12:09 -0400

    practicing exceptions

--- src/labs_examples/exception_handling/UnderageException.java
+++ src/labs_examples/exception_handling/UnderageException.java
@@ -1,2 +1,8 @@
-package labs_examples.exception_handling;public class Underage {
+package labs_examples.exception_handling;
+
+public class UnderageException extends Exception{
+    @Override
+    public String toString() {
+        return "Sorry kid, you must be 21 to purchase alcohol in the USA";
+    }
 }
--- src/labs_examples/exception_handling/labs/ABCStore.java
+++ src/labs_examples/exception_handling/labs/ABCStore.java
@@ -1,2 +1,31 @@
-package labs_examples.exception_handling.labs;public class ABCStore {
+package labs_examples.exception_handling.labs;
+
+import labs_examples.exception_handling.UnderageException;
+
+import java.util.Scanner;
+
+public class ABCStore {
+    public static void main(String[] args) {
+
+        try {
+            buyBeer();
+        } catch (UnderageException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    public static void buyBeer() throws UnderageException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Wake County ABC");
+        System.out.println("Are you over 21 years old? (y/n)");
+        String response = scanner.next();
+
+        if(response.equalsIgnoreCase("n")){
+            throw new UnderageException();
+        }else{
+            System.out.println("Great, what would you like to buy?");
+            //
+        }
+    }
 }
