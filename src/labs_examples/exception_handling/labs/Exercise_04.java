51435e981af2caab762218155a0c9913fd4fb73c
nicksinopoli <adsinopo@ncsu.edu>
2022-04-06 09:58:55 -0400

    nested try/catch

--- src/labs_examples/exception_handling/labs/Exercise_04.java
+++ src/labs_examples/exception_handling/labs/Exercise_04.java
@@ -6,3 +6,20 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a nested try/catch.
  *
  */
+
+class ExerciseFour{
+    public static void main(String[] args) {
+        int[] digits = { 2, 0, 14, 5, 22};
+        try {
+            int z = digits[9];
+        } catch (ArrayIndexOutOfBoundsException exc){
+            System.out.println("array exception caught");
+            try{
+                int aa = digits[2] / digits[1];
+            }catch (ArithmeticException ex){
+                System.out.println("bad math sir");
+            }
+            System.out.println("complete");
+        }
+    }
+}
