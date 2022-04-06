0e9c2baa6bb769c434f92a2622909d38e04e28e8
nicksinopoli <adsinopo@ncsu.edu>
2022-04-05 23:11:31 -0400

    exercise 3 finished

--- src/labs_examples/exception_handling/labs/Exercise_03.java
+++ src/labs_examples/exception_handling/labs/Exercise_03.java
@@ -6,3 +6,16 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a try/catch/finally block.
  *
  */
+
+class ExerciseThree {
+    public static void main(String[] args) {
+
+        try {
+            int x = 5 / 0;
+        } catch (ArithmeticException exc) {
+            System.out.println("Arithmetic Error caught");
+        } finally {
+            System.out.println("This will print no matter what");
+        }
+    }
+}
