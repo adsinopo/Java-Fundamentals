b05ddfe2968a955e04d6be2678a9000fd5bd9f7f
nicksinopoli <adsinopo@ncsu.edu>
2022-04-05 22:41:09 -0400

    exercise 2

--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -7,4 +7,20 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+class ExerciseTwo {
+    public static void main(String[] args) {
+        int[] array = {4, 6, 9, 12};
+
+        try {
+            int x = 4 / 2;
+            //this one will not print
+            int y = array[10];
+        } catch (ArithmeticException exc) {
+            System.out.println("Oops ArithmeticException Caught");
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Oops ArrayIndex caught");
+        }
+    }
+}
+
 
