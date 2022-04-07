1098560583f900f79cf75fcbc1231dcdcbb24e5c
nicksinopoli <adsinopo@ncsu.edu>
2022-04-07 12:48:27 -0400

    custom exceptions

--- src/labs_examples/exception_handling/labs/Driving.java
+++ src/labs_examples/exception_handling/labs/Driving.java
@@ -1,5 +1,7 @@
 package labs_examples.exception_handling.labs;
 
+import java.util.Scanner;
+
 /**
  * Exception Handling Exercise 7:
  *
@@ -8,6 +10,24 @@ package labs_examples.exception_handling.labs;
  */
 public class Driving {
     public static void main(String[] args) {
+        try{
+            ageTest();
+        } catch (AgeException exc){
+            System.out.println(exc.toString());
+        }
+    }
 
+    public static void ageTest() throws AgeException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the DMV");
+        System.out.println("Are you at least 16 years old? (y/n)");
+        String answer = scanner.next();
+
+        if(answer.equalsIgnoreCase("n")){
+            throw new AgeException();
+        } else {
+            System.out.println("The open road awaits!");
+        }
     }
 }
