029fe9d082194394b84897a9bef066ad8401baac
nicksinopoli <adsinopo@ncsu.edu>
2022-04-07 10:34:55 -0400

    throwing exceptions

--- src/labs_examples/exception_handling/labs/Exercise_05.java
+++ src/labs_examples/exception_handling/labs/Exercise_05.java
@@ -12,7 +12,24 @@ import java.util.Arrays;
 class Example {
 
     public static void main(String[] args) {
+        try{
+            mathCheck();
+        } catch (ArithmeticException exc){
+            System.out.println("printing in the main method");
+        }
+    }
 
+    public static void mathCheck() throws ArithmeticException{
+        anotherCheck();
+    }
+
+    public static void anotherCheck() throws ArithmeticException{
+        try{
+            int z = 4 / 0;
+        } catch (ArithmeticException exc){
+            System.out.println("Exception caught in anotherCheck");
+            throw exc;
+        }
     }
 
 }
